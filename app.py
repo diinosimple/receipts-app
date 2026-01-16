e0cd4017a4330ba5d68cb5c6194273e611027d98
diinosimple <diinosimple@gmail.com>
2026-01-16 17:58:06 +0900

    3rd deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import io
+import json
 import pickle
 from flask import Flask, request, render_template
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -10,28 +11,35 @@ from openpyxl import load_workbook
 app = Flask(__name__)
 
 # --- 環境変数 ---
-CREDENTIALS_FILE = os.environ.get('GOOGLE_CREDENTIALS_JSON', 'credentials.json')
-EXCEL_FILE_ID = os.environ['EXCEL_FILE_ID']  # Excel ファイルID
-RECEIPTS_FOLDER_ID = os.environ['RECEIPTS_FOLDER_ID']  # 個人 Drive 内アップロード先フォルダID
-
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
+EXCEL_FILE_ID = os.environ['EXCEL_FILE_ID']         # Excel ファイルID
+RECEIPTS_FOLDER_ID = os.environ['RECEIPTS_FOLDER_ID']  # 個人 Drive フォルダID
+GOOGLE_CREDENTIALS_JSON = os.environ['GOOGLE_CREDENTIALS_JSON']  # OAuth クライアント情報
 
-# --- OAuth 認証 ---
+TOKEN_FILE = 'token.pickle'
+
+
+# --- Drive サービス取得 ---
 def get_drive_service():
     creds = None
-    # token.pickle があれば読み込む
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    # 既存 token があれば読み込む
+    if os.path.exists(TOKEN_FILE):
+        with open(TOKEN_FILE, 'rb') as token:
             creds = pickle.load(token)
-    # なければ OAuth フロー
+
+    # 新規認証が必要な場合
     if not creds or not creds.valid:
-        flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
+        client_config = json.loads(GOOGLE_CREDENTIALS_JSON)
+        flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
         creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
+        with open(TOKEN_FILE, 'wb') as token:
             pickle.dump(creds, token)
+
     service = build('drive', 'v3', credentials=creds)
     return service
 
+
+# --- Flask ルート ---
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -46,7 +54,7 @@ def index():
         # --- 画像ファイル名作成 ---
         filename = f"{vendor} {date} ¥{int(amount):,}.jpg"
 
-        # --- Google Drive にアップロード ---
+        # --- Drive にアップロード ---
         media = MediaIoBaseUpload(file.stream, mimetype=file.mimetype)
         file_metadata = {
             "name": filename,
@@ -58,8 +66,7 @@ def index():
             fields='id'
         ).execute()
 
-        # --- Excel ファイル更新 ---
-        # Excel をダウンロード
+        # --- Excel 更新 ---
         request_dl = drive_service.files().get_media(fileId=EXCEL_FILE_ID)
         fh = io.BytesIO(request_dl.execute())
         wb = load_workbook(fh)
@@ -68,7 +75,6 @@ def index():
         out_fh = io.BytesIO()
         wb.save(out_fh)
         out_fh.seek(0)
-        # 上書きアップロード
         drive_service.files().update(
             fileId=EXCEL_FILE_ID,
             media_body=MediaIoBaseUpload(out_fh, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
@@ -76,8 +82,11 @@ def index():
 
         return f"アップロード完了: {filename}"
 
+    # GET は templates/index.html をレンダリング
     return render_template("index.html")
 
+
+# --- メイン ---
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
